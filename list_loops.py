86c6b85d153a3e91ebc4ddabbf77cfc8c7bcb2c4
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-09-29 12:01:11 -0700

    made a list of my own l
    added  an element, deleted an element, and made a loop

--- list_loops.py
+++ list_loops.py
@@ -13,3 +13,13 @@ songs.extend(["Savage mode", "ASTROTHUNDER"])
 print(songs)
 songs.remove("Do It")
 print(songs)
+
+animals = ["dogs", "cats", "lizard"]
+animals.append("birds")
+print(animals)
+print(animals[2])
+animals.remove("dogs")
+print(animals)
+
+for animal in animals:
+    print(animal)
